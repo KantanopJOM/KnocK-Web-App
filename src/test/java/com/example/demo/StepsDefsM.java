25a7341083422c9c6261fcff3bdd82044a216520
Sorasak PONGSAWAT <sorasak.po@ku.th>
2023-11-06 23:39:39 +0700

    edit signUp test

--- src/test/java/com/example/demo/StepsDefsM.java
+++ src/test/java/com/example/demo/StepsDefsM.java
@@ -1,34 +1,12 @@
 package com.example.demo;
 
-
-import com.example.demo.controller.ProductController;
-import com.example.demo.entity.Cart;
-import com.example.demo.entity.CartItem;
-import com.example.demo.entity.Product;
-import com.example.demo.service.CartItemService;
-import com.example.demo.service.CartService;
-import com.example.demo.service.ProductService;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import java.security.Principal;
 import java.util.UUID;
 
 public class StepsDefsM {
-
-    private Product product;
-    private Cart cart;
-    private CartItem cartItem;
-
-    private CartService cartService;
-
-    private CartItemService cartItemService;
-
-    private ProductService productService;
-
-    protected ProductController productController;
-
 
     @Given("the user is on the sign-up page")
     public void givenUserIsOnSignUpPage() {
@@ -65,16 +43,5 @@ public class StepsDefsM {
     public void thenUserRemainsOnSignUpPage() {
         // Verify the user is still on the sign-up page
     }
-
-    //add product to cart
-    @Given("the store is ready to service customers")
-    public void the_store_is_ready_to_service_customers() {
-
-    }
-
-    @Given("a product id {string}")
-    public void product_in_stock(UUID productId) {
-//        productController.addToCart(productId, principal);
-    }
-
+    
 }
